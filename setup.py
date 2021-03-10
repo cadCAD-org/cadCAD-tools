56b78a3e26e502fe871f702edfc245f23d18412a
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-03-10 18:03:54 -0300

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+long_description = """
+cadCAD tools
+"""
+
+setup(name='cadCAD_tools',
+      version='0.0.1.0',
+      description="tools",
+      long_description=long_description,
+      url='https://github.com/cadCAD-org/cadCAD-tools',
+      author='Danilo Lessa Bernardineli',
+      author_email='danilo@block.science, danilo.lessa@gmail.com',
+      packages=find_packages(),
+      install_requires=['cadCAD']
+)
